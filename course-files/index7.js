585fe2907c786545ef0fab8b4759baabbf2fd30b
gim1203-hue <gimrankhan1203@gmail.com>
2026-08-14 15:52:19 -0400

    update my html file

---
+++ course-files/index7.js
@@ -0,0 +1,58 @@
+console.log("Hello, World! my name is khan");
+
+// variables
+let name = "khan";
+let age = 40;
+console.log(name);
+console.log(age);
+
+const country = "usa";
+console.log(country);
+
+// let age1 = 26;
+// age1 = 30;
+
+let a = 15;
+let b = 20;
+let sum = a+b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+
+
+
+let name1 = "apples";
+let price = 5;
+let quantity = 10;
+let total = price * quantity;
+console.log(total);
+
+let name2 = "oranges";
+let price2 = 3;
+let quantity2 = 15;
+let total2 = price2 * quantity2;
+console.log(total2);
+
+let name3 = "bananas";
+let price3 = 2;
+let quantity3 = 20;
+let total3 = price3 * quantity3;
+console.log(total3);
+
+let total4 = total + total2 + total3;
+console.log(total4);
+
+let total5 = total4 - (total2 + total3);
+console.log(total5);
