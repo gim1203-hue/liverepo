30fe73f5367939b54bdda21bdedd261f184d1302
gim1203-hue <gimrankhan1203@gmail.com>
2026-08-14 21:02:52 -0400

    update my .html,.js,.css

---
+++ course-files/doc7/doc7.js
@@ -0,0 +1,102 @@
+console.log("Hello, World! my name is khan");
+let instructor = "Usman Saleem";
+console.log(instructor);
+console.log("instructor Usman Saleem");
+
+// variables
+console.log("name/age/gender/height/weight/religion/country");
+let name = "khan";
+let age = 40;
+let gender = "male";
+let height = 5.9;
+let weight = 190;
+let religion = "islam";
+console.log(name);
+console.log(age);
+console.log(gender);
+console.log(height);
+console.log(weight);
+console.log(religion);
+
+const country = "usa";
+console.log(country);
+
+// let age1 = 26;
+// age1 = 30;
+
+console.log("class1=15 students");
+console.log("class2=20 students");
+console.log("a/b/sum/difference/product/quotient/remainder");
+console.log("a")
+console.log("b")
+console.log("a + b");
+console.log("a - b");
+console.log("a * b");
+console.log("a / b");
+console.log("a % b");
+
+
+let a = 15;
+let b = 20;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+console.log(a)
+console.log(b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+
+console.log("name/price/quantity/pounds/total");
+let name1 = "apples";
+let price = "5";
+let quantity = 10;
+let pounds = "10pounds";
+let total = price * quantity;
+
+console.log(name1);
+console.log(price);
+console.log(quantity);
+console.log(pounds);
+console.log(total);
+
+
+let name2 = "oranges";
+let price2 = "3";
+console.log(price2);
+let quantity2 = 15;
+console.log(quantity2);
+let pounds2 = "10pounds";
+console.log(pounds2);
+let total2 = price2 * quantity2;
+console.log(total2);
+
+let name3 = "bananas";
+console.log(name3);
+let price3 = 4;
+console.log(price3);
+let quantity3 = 20;
+console.log(quantity3);
+let pounds3 = "10pounds";
+console.log(pounds3);
+let total3 = price3 * quantity3;
+console.log(total3);
+
+let name4 = "apples+oranges+bananas";
+console.log(name4);
+let total4 = total + total2 + total3;
+console.log(total4);
+
+let name5 = "total - (total2 + total3)";
+console.log(name5);
+let total5 = total4 - (total2 + total3);
+console.log(total5);
